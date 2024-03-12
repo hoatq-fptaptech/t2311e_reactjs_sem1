abb3112783ef7b1f2f466c776cc31acd6e8cda04
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-03-12 21:09:34 +0700

    list products

--- src/component/page/Home.js
+++ src/component/page/Home.js
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Product from "../common/Product";
 
 function Home(props){
     const [list,setList] = useState([]);
+    function callData(){
+        const url = `https://dummyjson.com/products?limit=12`;
+        fetch(url).then(rs=>rs.json())
+        .then(data=>{
+            setList(data.products);
+        });
+    }
+    useEffect(function(){
+        callData();
+    },[]);
     return (
-        <h1>Home Page content</h1>
+        <div>
+             <h1>Home Page content</h1>
+             {
+                list.map((e,i)=>{
+                    return <Product item={e} key={i}/>
+                })
+             }
+        </div>
     );
 }
 export default Home;
