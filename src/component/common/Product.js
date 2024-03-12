aa2678bf894aa6e6a94e2a0aa205667fa5ca9c7a
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-03-12 18:53:26 +0700

    demo call api

--- src/component/common/Product.js
+++ src/component/common/Product.js
@@ -1,11 +1,19 @@
-
-function Product(props){
+import { useState } from "react";
+function Product(props){ // component
     const item = props.item;
+    const [count,setCount] = useState(0);
+    function increment(){
+        setCount(count+1);
+        // setCount(count>0?count-1:0);
+    }
     return (
         <div className="col-4">
-            <h2>{item.name}</h2>
+            <img src={item.thumbnail} />
+            <h2>{item.title}</h2>
             <p>${item.price}</p>
             <p>{item.qty > 0 ? "In stock": "Out of stock"}</p>
+            <h2>Count: {count}</h2>
+            <button onClick={increment} className="btn btn-primary" type="button">Increment</button>
         </div>
     );
 }
--- src/component/page/ProductDetail.js
+++ src/component/page/ProductDetail.js
@@ -1,14 +1,21 @@
 import Product from "../common/Product";
-
+import { useState , useEffect } from "react";
 function ProductDetail(props){
-    const item = {
-        name: "Iphone 15 Promax",
-        price: 1200,
-        qty: 5
+    const [item,setItem] = useState({}); // khởi tạo 1 state
+    function callData(){
+        const url = `https://dummyjson.com/products/2`;
+        fetch(url).then(rs=>rs.json())
+        .then(data=>{
+            setItem(data);
+        })
     }
+    useEffect(function(){ // setTimeOut
+        callData();
+    },[]);
     return  (
        <div>
             <h1>Product Detail</h1>
+        
             <Product item={item}/>
        </div>     
     );
